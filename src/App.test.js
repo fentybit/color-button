0955b2408026e52f5437e751882bafe0a81879f6
fentybit <fentymhall@gmail.com>
2022-10-29 10:44:15 -0700

    test behavior when clicking button

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import {logRoles } from '@testing-library/dom'
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('button has correct initial color', () => {
-    const {container} = render(<App />);
+test('button has correct initial color, and updates when clicked', () => {
+    render(<App />);
 
-    logRoles(container)
+    const colorButton = screen.getByRole('button', {name: 'Change to blue'});
 
-    const colorButton = screen.getByRole('button', {name: 'Change to blue'})
+    expect(colorButton).toHaveStyle({backgroundColor: 'red'});
 
-    expect(colorButton).toHaveStyle({backgroundColor: 'red'})
+    fireEvent.click(colorButton);
+
+    expect(colorButton).toHaveStyle({backgroundColor: 'blue'});
+
+    expect(colorButton).toHaveTextContent('Change to red');
 });
-
-test('button has correct initial test', () => {
-
-})
-
-
